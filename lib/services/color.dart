603bf4d27bb44862783adc23f829553ade40914f
msayed6789 <mohamed.sayed.abdelrhman@gmail.com>
2025-06-07 14:17:57 +0300

    App theme done

--- lib/services/color.dart
+++ lib/services/color.dart
@@ -2,4 +2,6 @@ class ColorsApp {
   static int black = 0xFF202020;
   static int gold = 0xFFE2BE7F;
   static int lightGold = 0xFF707070;
+  static int offWhit = 0xFFFBF1DF;
+  static int brown = 0xFF916006;
 }
